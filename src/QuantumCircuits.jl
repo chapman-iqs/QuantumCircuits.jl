084f97d316991055bac944634b1592e48ca1ebc2
Sacha <sachagreenfield@gmail.com>
2022-06-24 16:15:00 -0700

    Modified fidelity method to fix bug; QuantumOpticsBase.fidelity needs dense operators as arguments. Moved fidelity and expectations into utils.jl.

--- src/QuantumCircuits.jl
+++ src/QuantumCircuits.jl
@@ -24,7 +24,7 @@ const tt = 0.0 # Timescale
 const rr = [0.0] # Readout
 const ρρ = identityoperator(SpinBasis(1//2)) # State
 
-δ(i,j) = Int(i == j);
+
 
 # holds solutions to bayesian or rouchon (rouchon not implemented)
 struct Solution
@@ -33,39 +33,8 @@ struct Solution
 	r::Vector{Record}
 end
 
-"""
-	expectations(sol::Solution, op::State)
-	expectations(ρs::Vector{State}, op::State)
+include("utils.jl")
 
-Gives a time series of expectation values of op for the density matrices sol.ρ or ρs.
-
-### Returns :: Vector{Float64}
-"""
-function expectations(ρs::Vector{State}, op::QOp)
-	map(state -> real(expect(op, state)), ρs)
-end
-function expectations(ρs::Vector{State}, op::Ket)
-	map(state -> real(expect(dm(op), state)), ρs)
-end
-function expectations(sol::Solution, op)
-	expectations(sol.ρ, op)
-end
-
-
-"""
-	fidelity(ρ::State, σ::State)
-
-Gives the fidelity of two quantum states, ρ and σ, using the standard formula Tr(√(√σ ρ √σ)).
-Uses the built-in QuantumOpticsBase.fidelity, but catches cases where ρ or σ may be a Ket,
-and makes the appropriate conversion.
-
-### Returns:
-  - Tr(√(√σ ρ √σ)) :: Float64
-"""
-fidelity(ρ::Ket, σ::Ket) = real(ρ' * σ)^2
-fidelity(ρ::Ket, σ::QOp) = real(fidelity(dm(ρ), σ))
-fidelity(ρ::QOp, σ::Ket) = real(fidelity(ρ, dm(σ)))
-fidelity(ρ::QOp, σ::QOp) = real(QuantumOpticsBase.fidelity(ρ, σ))
 
 
 """
---
+++ src/utils.jl
@@ -0,0 +1,36 @@
+
+"""
+	fidelity(ρ::State, σ::State)
+
+Gives the fidelity of two quantum states, ρ and σ, using the standard formula Tr(√(√σ ρ √σ)).
+Uses the built-in QuantumOpticsBase.fidelity, but catches cases where ρ or σ may be a Ket,
+and makes the appropriate conversion.
+
+### Returns:
+  - Tr(√(√σ ρ √σ)) :: Float64
+"""
+fidelity(ρ::Ket, σ::Ket) = real(ρ' * σ)^2
+fidelity(ρ::Ket, σ::QOp) = real(fidelity(dm(ρ), σ))
+fidelity(ρ::QOp, σ::Ket) = real(fidelity(ρ, dm(σ)))
+fidelity(ρ::QOp, σ::QOp) = real(QuantumOpticsBase.fidelity(DenseOperator(ρ), DenseOperator(σ)))
+
+"""
+	expectations(sol::Solution, op::State)
+	expectations(ρs::Vector{State}, op::State)
+
+Gives a time series of expectation values of op for the density matrices sol.ρ or ρs.
+
+### Returns :: Vector{Float64}
+"""
+function expectations(ρs::Vector{State}, op::QOp)
+	map(state -> real(expect(op, state)), ρs)
+end
+function expectations(ρs::Vector{State}, op::Ket)
+	map(state -> real(expect(dm(op), state)), ρs)
+end
+function expectations(sol::Solution, op)
+	expectations(sol.ρ, op)
+end
+
+"Kronecker delta function"
+δ(i,j) = Int(i == j);
